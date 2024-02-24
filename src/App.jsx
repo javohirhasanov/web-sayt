5756f9e3a7eee79a51d20c693661f2fec6d119c5
javohirhasanov <hasanovjavohir1707@gmail.com>
2024-02-24 15:48:46 +0500

    sayt

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
 
-    <div>Javohir</div>
+    <div>Javohir
+      hasanov
+    </div>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
