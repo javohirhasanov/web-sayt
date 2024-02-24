08a7f4a429f852ddd9ce59fa419e0f377f87626e
javohirhasanov <hasanovjavohir1707@gmail.com>
2024-02-24 15:52:52 +0500

    oxirgi uzgatirish

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
 
        <h1>Javohir</h1>
+       <h2>Salom</h2>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
